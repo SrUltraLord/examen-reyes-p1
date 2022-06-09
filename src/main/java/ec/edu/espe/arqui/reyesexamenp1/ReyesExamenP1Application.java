32b75caca7fa6f4391734348d564f2b81524495b
David Reyes <dareyes6@espe.edu.ec>
2022-06-08 19:25:29 -0500

    First Commit

---
+++ src/main/java/ec/edu/espe/arqui/reyesexamenp1/ReyesExamenP1Application.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqui.reyesexamenp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReyesExamenP1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReyesExamenP1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/espe/arqui/reyesexamenp1/ReyesExamenP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqui.reyesexamenp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReyesExamenP1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
